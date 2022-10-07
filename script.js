aa0ae11267350c74a2f766158b6e056346b60d64
Denis <hallo-boy@mail.ru>
2022-10-07 09:04:40 +0300

    finish

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-
-
-
 let quoteArray = [];
-let index = 0; 
-let textPosition = 0; 
+let index = 0;
+let textPosition = 0;
 let flag = true;
 
 loadQuote = () => {
-  const url = 'https://fish-text.ru/get';
+  const url = "https://fish-text.ru/get?&number=1";
 
   fetch(url)
-
-  .then(response => {
-    if (!response.ok) throw Error(response.statusText);
+    .then((response) => {
+      if (!response.ok) throw Error(response.statusText);
       return response.json();
-   })
+    })
 
-   .then(data => {
+    .then((data) => {
       quoteArray[index] = data.text;
-   })
+    })
 
-   .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
 
 typewriter = () => {
-  if(flag){
+  if (flag) {
     loadQuote();
-    quoteArray[index] += ""; 
+    quoteArray[index] += "";
     flag = false;
   }
 
-  document.querySelector(".container").innerHTML = quoteArray[index].substring(0, textPosition) + '<span>\u25AE</span>';
+  document.querySelector(".container").innerHTML =
+    quoteArray[index].substring(0, textPosition) + "<span>\u25AE</span>";
 
-  if(textPosition++ != quoteArray[index].length){
+  if (textPosition++ != quoteArray[index].length) {
     setTimeout("typewriter()", 100);
-  }
-  else{
-    quoteArray[index] = ' ';
+  } else {
+    quoteArray[index] = " ";
     setTimeout("typewriter()", 4000);
     textPosition = 0;
     flag = true;
-  }   
-}
+  }
+};
 
-window.addEventListener('load', typewriter);
+window.addEventListener("load", typewriter);
